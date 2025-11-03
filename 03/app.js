7130327b5f70076da13e2320943a9c9cf5fc6f68
Zaneta Sochon <twj email>
2025-11-03 17:53:02 +0100

    feat(3): Calculate sum of the occupied space

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,36 @@
 import {files} from './data.js';
 
+
+const result = () => {
+calculateUnits()
+    }
+
+    const calculateUnits = () => {
+        let unitsSum = 0;
+           
+     files.forEach(el => {
+        const size = el.size.length
+        const unit = el.size.unit
+
+    if(!unit){
+        unitsSum += size
+    }
+
+    if(unit === 'KB'){
+    unitsSum += size * 1024   
+
+    } 
+
+    if(unit === 'MB'){
+        unitsSum += size * 1024 * 1024
+    }
+
+    if(unit === 'GB') {
+        unitsSum += size * 1024 * 1024 * 1024
+    }
+  });
+   console.log(`Suma przestrzeni zajmowanej przez wszystkie pliki to ${unitsSum}`); }
+
+
+
+result()
