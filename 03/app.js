9375978a3da680c5430fe0444b234c49bbf91c5f
Zaneta Sochon <twj email>
2025-11-04 19:49:11 +0100

    fix(3): Refactor

--- 03/app.js
+++ 03/app.js
@@ -6,28 +6,16 @@ calculateUnits()
     }
 
     const calculateUnits = () => {
+        const unitsMap = {"B": 1024 ** 0, "KB": 1024 ** 1, "MB": 1024 ** 2, "GB": 1024 ** 3, "TB": 1024 ** 4, "PB": 1024 ** 5, "EB": 1024 ** 6, "ZB": 1024 ** 7, "YB": 1024 ** 8}
+            
         let unitsSum = 0;
            
      files.forEach(el => {
         const size = el.size.length
         const unit = el.size.unit
+   
 
-    if(!unit){
-        unitsSum += size
-    }
-
-    if(unit === 'KB'){
-    unitsSum += size * 1024   
-
-    } 
-
-    if(unit === 'MB'){
-        unitsSum += size * 1024 * 1024
-    }
-
-    if(unit === 'GB') {
-        unitsSum += size * 1024 * 1024 * 1024
-    }
+       unitsSum += unit ? size * unitsMap[unit] : size * unitsMap.B
   });
    console.log(`Suma przestrzeni zajmowanej przez wszystkie pliki to ${unitsSum}`); }
 
