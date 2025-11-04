b4f73b498a2a54130dbdbad61aa545a880ee80ce
Zaneta Sochon <twj email>
2025-11-04 19:54:11 +0100

    fix(5): Refactor redundancy

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,35 +1,7 @@
-export default class Admin {
-    constructor( {login = '', password = ''} ) {
-        this.login = login;
-        this.password = password;
-    }
+import User from "./User"
 
-    register() {
-        if(this.isValid()) {
-            console.log('done');
-            return true;
-        }
 
-        console.log('error');
-        return false;
-    }
-
-    isValid() {
-        if(this.isLoginCorrect() && this.isPasswordCorrect()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    isLoginCorrect() {
-        if(this.login.includes('@')) {
-            return true;
-        }
-
-        return false;
-    }
-
+export class Admin extends User {
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
